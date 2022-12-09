0387e43a10ac39b819823f8f61ddafb0e318a651
root <root@31991.hostserv.eu>
2022-12-09 18:11:10 +0100

    settings

--- huebhof/settings.py
+++ huebhof/settings.py
@@ -132,7 +132,6 @@ if DEBUG is True:
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 IMPERSONATE = {
     'REDIRECT_URL': '/my/profile',
