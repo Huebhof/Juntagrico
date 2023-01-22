c2f538d9fdaf4497123e72975b7f5d12ad718550
ringloop <beat.wuersten@gmail.com>
2023-01-22 19:10:24 +0100

    modifie select_start_date.html

--- huebhof/apps.py
+++ huebhof/apps.py
@@ -8,3 +8,24 @@ class HuebhofConfig(AppConfig):
 ​
     def ready(self):
         from juntagrico.config import Config
+​
+        def agb_label():
+            documents = {
+                'den Abovertrag': Config.extra_sub_info,
+            }
+            documents_html = []
+            for text, link in documents.items():
+                if link().strip():
+                    documents_html.append('<a target="_blank" href="{}">{}</a>'.format(link(), _(text)))
+            if documents_html:
+                return _('Ich habe {} gelesen und erkläre meinen Willen, "{}" beizutreten. '
+                         'Hiermit beantrage ich meine Aufnahme.').format(
+                    (' ' + _('und') + ' ').join(documents_html),
+                    Config.organisation_long_name()
+                )
+            else:
+                return _('Ich erkläre meinen Willen, "{}" beizutreten. '
+                         'Hiermit beantrage ich meine Aufnahme.').format(Config.organisation_long_name())
+​
+        from juntagrico.forms import RegisterMemberForm
+        RegisterMemberForm.agb_label = staticmethod(agb_label)
